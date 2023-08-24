630ac7d957be2a2818ad8c3d4f92e6cf7a594abc
Andres Rodriguez <contact@andresr.rocks>
2023-08-24 09:38:14 -0400

    removed embedded graphics since it doesnt support alpha channel

--- embedded-graphics/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cargo psp
-PPSSPPSDL ./target/mipsel-sony-psp/debug/psp-embedded-graphics-example.EBOOT.PBP
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-cargo psp
-PPSSPPQt ./target/mipsel-sony-psp/debug/psp-embedded-graphics-example.EBOOT.PBP
+OPENSSL_DIR=/usr/ cargo psp && PPSSPPQt ./target/mipsel-sony-psp/debug/psp-embedded-graphics-example.EBOOT.PBP
