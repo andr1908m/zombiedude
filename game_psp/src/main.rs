59fa6fbe66727c063fb1dccdaa7c2784dc97a780
Andres Rodriguez <contact@andresr.rocks>
2023-08-26 11:55:32 -0400

    basic sceGu template

--- game_psp/src/main.rs
+++ game_psp/src/main.rs
@@ -1,26 +1,11 @@
 #![no_std]
 #![no_main]
 
-use core::{ptr, f32::consts::PI};
-use psp::Align16;
-use psp::sys::{
-    self, ScePspFVector3, DisplayPixelFormat, GuContextType, GuSyncMode, GuSyncBehavior,
-    GuPrimitive, TextureFilter, TextureEffect, TextureColorComponent,
-    FrontFaceDirection, ShadingModel, GuState, TexturePixelFormat, DepthFunc,
-    VertexType, ClearBuffer, MipmapLevel,
-};
-use psp::vram_alloc::{get_vram_allocator, VramMemChunk};
-use psp::{BUF_WIDTH, SCREEN_WIDTH, SCREEN_HEIGHT};
+use core::ffi::c_void;
+
+use psp::{sys::{self, TexturePixelFormat, sceGeEdramGetAddr, sceGuInit, GuContextType, GuState, DepthFunc, FrontFaceDirection, ShadingModel, GuSyncMode, GuSyncBehavior, sceGuDisplay, rgba, ClearBuffer}, Align16, vram_alloc::get_vram_allocator};
 
 psp::module!("sample_cube", 1, 1);
-
-// Both width and height, this is a square image.
-const IMAGE_SIZE: usize = 128;
-
-// The image data *must* be aligned to a 16 byte boundary.
-static FERRIS: Align16<[u8; IMAGE_SIZE * IMAGE_SIZE * 4]> = Align16(*include_bytes!("../assets/ferris.bin"));
-
-static mut LIST: Align16<[u32; 0x40000]> = Align16([0; 0x40000]);
 
 #[repr(C, align(4))]
 struct Vertex {
@@ -31,166 +16,166 @@ struct Vertex {
     z: f32,
 }
 
-static VERTICES: Align16<[Vertex; 12 * 3]> = Align16([
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z:  1.0}, // 0
-    Vertex { u: 1.0, v: 0.0, x: -1.0, y:  1.0, z:  1.0}, // 4
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 5
+const BUFFER_WIDTH: i32 = 512;
+const SCREEN_WIDTH: i32 = 480;
+const SCREEN_HEIGHT: i32 = 272;
 
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z:  1.0}, // 0
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 5
-    Vertex { u: 0.0, v: 1.0, x:  1.0, y: -1.0, z:  1.0}, // 1
+static mut LIST: Align16<[u32; 0x40000]> = Align16([0; 0x40000]);
 
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 3
-    Vertex { u: 1.0, v: 0.0, x:  1.0, y: -1.0, z: -1.0}, // 2
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z: -1.0}, // 6
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 3
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z: -1.0}, // 6
-    Vertex { u: 0.0, v: 1.0, x: -1.0, y:  1.0, z: -1.0}, // 7
-
-    Vertex { u: 0.0, v: 0.0, x:  1.0, y: -1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 0.0, x:  1.0, y: -1.0, z:  1.0}, // 3
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 7
-
-    Vertex { u: 0.0, v: 0.0, x:  1.0, y: -1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 7
-    Vertex { u: 0.0, v: 1.0, x:  1.0, y:  1.0, z: -1.0}, // 4
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 0.0, x: -1.0, y:  1.0, z: -1.0}, // 3
-    Vertex { u: 1.0, v: 1.0, x: -1.0, y:  1.0, z:  1.0}, // 7
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 1.0, x: -1.0, y:  1.0, z:  1.0}, // 7
-    Vertex { u: 0.0, v: 1.0, x: -1.0, y: -1.0, z:  1.0}, // 4
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y:  1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 0.0, x:  1.0, y:  1.0, z: -1.0}, // 1
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 2
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y:  1.0, z: -1.0}, // 0
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y:  1.0, z:  1.0}, // 2
-    Vertex { u: 0.0, v: 1.0, x: -1.0, y:  1.0, z:  1.0}, // 3
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 4
-    Vertex { u: 1.0, v: 0.0, x: -1.0, y: -1.0, z:  1.0}, // 7
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y: -1.0, z:  1.0}, // 6
-
-    Vertex { u: 0.0, v: 0.0, x: -1.0, y: -1.0, z: -1.0}, // 4
-    Vertex { u: 1.0, v: 1.0, x:  1.0, y: -1.0, z:  1.0}, // 6
-    Vertex { u: 0.0, v: 1.0, x:  1.0, y: -1.0, z: -1.0}, // 5
-]);
+fn list() -> *mut c_void {
+  unsafe { &mut LIST as *mut _ as *mut c_void }
+}
 
 fn psp_main() {
-    unsafe { psp_main_inner() }
-}
+  psp::enable_home_button();
 
-unsafe fn psp_main_inner() {
-    psp::enable_home_button();
+  initialize_graphics();
+  let mut running = true;
+  while running {
+    start_frame();
 
-    // Attempting to free the three VRAM chunks at this point would give a
-    // compile-time error since fbp0, fbp1 and zbp are used later on
-    //allocator.free_all();
-
-    init_graphics();
-
-    let mut val = 0.0;
-
-    loop {
-        sys::sceGuStart(GuContextType::Direct, &mut LIST.0 as *mut [u32; 0x40000] as *mut _);
-
-        // clear screen
-        sys::sceGuClearColor(0xff554433);
-        sys::sceGuClearDepth(0);
-        sys::sceGuClear(ClearBuffer::COLOR_BUFFER_BIT | ClearBuffer::DEPTH_BUFFER_BIT);
-
-        // setup matrices for cube
-
-        sys::sceGumMatrixMode(sys::MatrixMode::Projection);
-        sys::sceGumLoadIdentity();
-        sys::sceGumPerspective(75.0, 16.0 / 9.0, 0.5, 1000.0);
-
-        sys::sceGumMatrixMode(sys::MatrixMode::View);
-        sys::sceGumLoadIdentity();
-
-        sys::sceGumMatrixMode(sys::MatrixMode::Model);
-        sys::sceGumLoadIdentity();
-
-        {
-            let pos = ScePspFVector3 { x: 0.0, y: 0.0, z: -2.5 };
-            let rot = ScePspFVector3 {
-                x: val * 0.79 * (PI / 180.0),
-                y: val * 0.98 * (PI / 180.0),
-                z: val * 1.32 * (PI / 180.0),
-            };
-
-            sys::sceGumTranslate(&pos);
-            sys::sceGumRotateXYZ(&rot);
-        }
-
-        // setup texture
-
-        sys::sceGuTexMode(TexturePixelFormat::Psm8888, 0, 0, 0);
-        sys::sceGuTexImage(MipmapLevel::None, 128, 128, 128, &FERRIS as *const _ as *const _);
-        sys::sceGuTexFunc(TextureEffect::Replace, TextureColorComponent::Rgb);
-        sys::sceGuTexFilter(TextureFilter::Linear, TextureFilter::Linear);
-        sys::sceGuTexScale(1.0, 1.0);
-        sys::sceGuTexOffset(0.0, 0.0);
-
-        // draw cube
-
-        sys::sceGumDrawArray(
-            GuPrimitive::Triangles,
-            VertexType::TEXTURE_32BITF | VertexType::VERTEX_32BITF | VertexType::TRANSFORM_3D,
-            12 * 3,
-            ptr::null_mut(),
-            &VERTICES as *const Align16<_> as *const _,
-        );
-
-        sys::sceGuFinish();
-        sys::sceGuSync(GuSyncMode::Finish, GuSyncBehavior::Wait);
-
-        sys::sceDisplayWaitVblankStart();
-        sys::sceGuSwapBuffers();
-
-        val += 1.0;
+    unsafe {
+      sys::sceGuClearColor(rgba(0x00, 0xFF, 0x00, 0xFF));
+      sys::sceGuClear(ClearBuffer::COLOR_BUFFER_BIT | ClearBuffer::DEPTH_BUFFER_BIT);
     }
 
-    // sys::sceGuTerm();
-    // psp::sys::sceKernelExitGame();
+    end_frame();
+  }
+  terminate_graphics();
+
+  exit();
 }
 
-unsafe fn init_graphics() {
-    let mut allocator = get_vram_allocator().unwrap();
-    let fbp0 = allocator.alloc_texture_pixels(BUF_WIDTH, SCREEN_HEIGHT, TexturePixelFormat::Psm8888);
-    let fbp1 = allocator.alloc_texture_pixels(BUF_WIDTH, SCREEN_HEIGHT, TexturePixelFormat::Psm8888);
-    let zbp = allocator.alloc_texture_pixels(BUF_WIDTH, SCREEN_HEIGHT, TexturePixelFormat::Psm4444);
-  
-    sys::sceGumLoadIdentity();
+fn initialize_graphics() {
+  let (draw_buff, disp_buff, depth_buff) = create_graphics_buffers();
 
-    sys::sceGuInit();
-    
+  unsafe {  
+    start_gu();
+    start_display_context(draw_buff, disp_buff, depth_buff);
+    set_viewport();
+    set_depth_range();
+    enable_scissors();
+    enable_depth_test();
+    enable_cull_face();
+    enable_smooth_shading();
+    enable_textures();
+    send_to_gu();
+    enable_display();
+  }
+}
 
-    sys::sceGuStart(GuContextType::Direct, &mut LIST.0 as *mut [u32; 0x40000] as *mut _);
-    sys::sceGuDrawBuffer(DisplayPixelFormat::Psm8888, fbp0.as_mut_ptr_from_zero() as _, BUF_WIDTH as i32);
-    sys::sceGuDispBuffer(SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32, fbp1.as_mut_ptr_from_zero() as _, BUF_WIDTH as i32);
-    sys::sceGuDepthBuffer(zbp.as_mut_ptr_from_zero() as _, BUF_WIDTH as i32);
-    sys::sceGuOffset(2048 - (SCREEN_WIDTH / 2), 2048 - (SCREEN_HEIGHT / 2));
-    sys::sceGuViewport(2048, 2048, SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32);
-    sys::sceGuDepthRange(65535, 0);
-    sys::sceGuScissor(0, 0, SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32);
-    sys::sceGuEnable(GuState::ScissorTest);
-    sys::sceGuDepthFunc(DepthFunc::GreaterOrEqual);
-    sys::sceGuEnable(GuState::DepthTest);
-    sys::sceGuFrontFace(FrontFaceDirection::Clockwise);
-    sys::sceGuShadeModel(ShadingModel::Smooth);
-    sys::sceGuEnable(GuState::CullFace);
-    sys::sceGuEnable(GuState::Texture2D);
-    sys::sceGuEnable(GuState::ClipPlanes);
+fn terminate_graphics() {
+  unsafe { sys::sceGuTerm() };
+}
+
+fn start_frame() {
+  unsafe { sys::sceGuStart(GuContextType::Direct, list()) };
+}
+
+fn end_frame() {
+  unsafe { 
     sys::sceGuFinish();
     sys::sceGuSync(GuSyncMode::Finish, GuSyncBehavior::Wait);
-
-    psp::sys::sceDisplayWaitVblankStart();
-
-    sys::sceGuDisplay(true);
+    sys::sceDisplayWaitVblankStart();
+    sys::sceGuSwapBuffers();
+  };
 }
+
+fn enable_display() {
+  unsafe { sys::sceGuDisplay(true) };
+}
+
+fn send_to_gu() {
+  unsafe {
+    sys::sceGuFinish();
+    sys::sceGuSync(GuSyncMode::Finish, GuSyncBehavior::Wait); // blocking call
+    sys::sceDisplayWaitVblankStart();
+  }
+}
+
+fn create_graphics_buffers() -> (*mut c_void, *mut c_void, *mut c_void) {
+  let mut allocator = get_vram_allocator().unwrap();
+  let bwi32 = BUFFER_WIDTH as u32;
+  let shi32 = SCREEN_HEIGHT as u32;
+  let draw_buff = allocator
+    .alloc_texture_pixels(bwi32, shi32, TexturePixelFormat::Psm8888)
+    .as_mut_ptr_from_zero() as *mut c_void;
+  let disp_buff = allocator
+    .alloc_texture_pixels(bwi32, shi32, TexturePixelFormat::Psm8888)
+    .as_mut_ptr_from_zero() as *mut c_void;
+  let depth_buff = allocator
+    .alloc_texture_pixels(bwi32, shi32, TexturePixelFormat::Psm4444)
+    .as_mut_ptr_from_zero() as *mut c_void;
+
+  (draw_buff, disp_buff, depth_buff)
+}
+
+fn start_gu() {
+  unsafe { sys::sceGuInit() };
+}
+
+fn start_display_context(draw_buff: *mut c_void, disp_buff: *mut c_void, depth_buff: *mut c_void) {
+  unsafe {
+    sys::sceGuStart(GuContextType::Direct, list());
+    sys::sceGuDrawBuffer(sys::DisplayPixelFormat::Psm4444, draw_buff, BUFFER_WIDTH);
+    sys::sceGuDispBuffer(SCREEN_WIDTH, SCREEN_HEIGHT, disp_buff, BUFFER_WIDTH);
+    sys::sceGuDepthBuffer(depth_buff, BUFFER_WIDTH);
+  }
+}
+
+fn set_viewport() {
+  let x = 2048 - (SCREEN_WIDTH / 2) as u32;
+  let y = 2048 - (SCREEN_HEIGHT / 2) as u32;
+  unsafe {
+    sys::sceGuOffset(x, y);
+    sys::sceGuViewport(2048, 2048, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+}
+
+fn set_depth_range() {
+  unsafe {
+    sys::sceGuDepthRange(65535, 0);
+  }
+}
+
+fn enable_scissors() {
+  unsafe {
+    sys::sceGuEnable(GuState::ScissorTest);
+    sys::sceGuScissor(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+}
+
+/// Things further away should be clipped
+fn enable_depth_test() {
+  unsafe { 
+    sys::sceGuEnable(GuState::DepthTest);
+    sys::sceGuDepthFunc(DepthFunc::GreaterOrEqual);
+  };
+}
+
+fn enable_cull_face() {
+  unsafe { 
+    sys::sceGuEnable(GuState::CullFace);
+    sys::sceGuFrontFace(FrontFaceDirection::Clockwise);
+  };
+}
+
+fn enable_textures() {
+  unsafe {
+    sys::sceGuEnable(GuState::Texture2D);
+    sys::sceGuEnable(GuState::ClipPlanes);
+  }
+}
+
+fn enable_smooth_shading() {
+  unsafe {
+    sys::sceGuShadeModel(ShadingModel::Smooth);
+  }
+}
+
+fn exit() {
+  unsafe {
+    sys::sceKernelExitGame();
+  }
+}
+
