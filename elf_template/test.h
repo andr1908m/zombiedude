f8f848586d830d4eee83c06082abfaa1f1c55f5d
Andres Rodriguez <contact@andresr.rocks>
2023-09-30 00:35:34 -0400

    remove all this stuff

--- elf_template/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-#include <stddef.h>
-
-size_t add(size_t left, size_t right);
-
-#endif
--- rust-and-c/src/test.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-#include <stdio.h>
-
-int main(int argc, char *argv[]) { 
-  printf("%d = 3\n", add(1, 2));
-}
