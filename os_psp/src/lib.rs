76516df3720574d8180e21295758d376bbfe31a8
Andres Rodriguez <contact@andresr.rocks>
2023-09-30 00:41:12 -0400

    remove more unnecessary files

--- os_psp/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![feature(macro_metavar_expr)]
-#![no_std]
-
-pub mod c_runtime;
-pub mod psp_module;
-pub mod macros;
--- ppsspp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-RUST_BACKTRACE=1 PPSSPPQt ./target/mipsel-sony-psp/debug/EBOOT.PBP
